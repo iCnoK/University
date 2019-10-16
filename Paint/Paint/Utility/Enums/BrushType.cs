f7f1744c9012838e47eea98451515f919e7079ab
iCnoK <krutoi_andriei@mail.ru>
2019-10-16 19:42:32 +0300

    1.0

---
+++ Paint/Paint/Utility/Enums/BrushType.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paint.Utility.Enums
+{
+    /// <summary>
+    /// Тип кисти
+    /// </summary>
+    public enum BrushType
+    {
+        /// <summary>
+        /// Маркер
+        /// </summary>
+        MARKER,
+        /// <summary>
+        /// Перьевая ручка
+        /// </summary>
+        FOUNTAINPEN,
+        /// <summary>
+        /// Кисть для масляных красок
+        /// </summary>
+        OILBRUSH,
+        /// <summary>
+        /// Акварель
+        /// </summary>
+        WATERCOLOR,
+        /// <summary>
+        /// Пиксельное перо
+        /// </summary>
+        PIXELPEN,
+        /// <summary>
+        /// Карандаш
+        /// </summary>
+        PENCIL,
+        /// <summary>
+        /// Ластик
+        /// </summary>
+        ERASER,
+        /// <summary>
+        /// Пастель
+        /// </summary>
+        PASTEL,
+        /// <summary>
+        /// Баллончик с краской
+        /// </summary>
+        SPRAYCAN,
+        /// <summary>
+        /// Заполнить
+        /// </summary>
+        FILL
+    }
+}
